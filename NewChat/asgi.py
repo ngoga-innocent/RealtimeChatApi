9833f0c62a517d6b35335847dc28134df3804735
Ngoga Innocent Patrick <ngogainnocent1@gmail.com>
2025-02-09 10:00:14 +0200

    Fix Django not loading apps in ASGI

--- NewChat/asgi.py
+++ NewChat/asgi.py
@@ -1,5 +1,10 @@
 
 import os
+
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NewChat.settings")  # Replace with your project name
+django.setup()
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 from chat.routing import wsPattern
