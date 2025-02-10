86b62168b2dbfacda8a460ded846a59431d1819a
Ngoga Innocent Patrick <ngogainnocent1@gmail.com>
2025-02-10 10:53:29 +0200

    csrf

--- NewChat/settings.py
+++ NewChat/settings.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = ['*']  # OR ['192.168.8.101', 'localhost']
 
 INSTALLED_APPS = [
     'daphne',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -45,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True 
 
 ROOT_URLCONF = 'NewChat.urls'
 REST_FRAMEWORK = {
